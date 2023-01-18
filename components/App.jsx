b95850d43f734323b5d48cc7a291acb86e638211
c-catanese <111301202+c-catanese@users.noreply.github.com>
2023-01-18 11:52:01 -0500

    merging changes so jason can work on the endpoints and the routing

--- components/App.jsx
+++ components/App.jsx
@@ -1,19 +1,20 @@
 import LoginPage from "./LoginPage.jsx";
 import React from "react";
 import SideBar from "./SideBar.jsx";
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 const App = () => {
-
   return(
-    <>
-      <LoginPage /> 
-      <SideBar />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path = "/" element = {<LoginPage />}/>
+        <Route exact path = "/home" element = {<SideBar />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 
 export default App;
