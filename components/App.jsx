fe3711a6b9a9769830673a5631bedf58859eaa86
mariamzakariadze <mariazakariadze@gmail.com>
2023-01-18 15:10:07 -0500

    navigation done

--- components/App.jsx
+++ components/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<LoginPage />} />
-        <Route path="/home" element={<SideBar />} />
+        <Route path="/home"  element={<SideBar />} />
       </Routes>
     </BrowserRouter>
   );
--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -4,6 +4,8 @@ import {Button, Typography, Box, TextField} from '@mui/material';
 import "../styling/login.css";
 import { useDispatch, useSelector } from 'react-redux';
 import {updateUserPassActionCreator, updateSignInActionCreator, loginActionCreator} from "../redux/actions.js";
+import { Link, Navigate } from "react-router-dom";
+import { useNavigate } from 'react-router';
 
 
 
@@ -12,7 +14,9 @@ const LoginPage =  () => {
   const usernameState = useSelector(state => state.auth.username);
   const passwordState = useSelector(state => state.auth.password);
   const showSignUp = useSelector(state => state.auth.showSignup);
+  const [confirmPassword,setConfirmPassword] = useState('')
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
 
   function handleSubmit(e) {
@@ -23,12 +27,35 @@ const LoginPage =  () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({username: usernameState, password: passwordState})
-    }).then((res) => res.json())
+    }).then(res => {
+       if (res.status == 200) {
+        navigate("/home")
+        }})
     .then((data)=>{
       console.log('THIS IS FROM THE RESPONSE', data)
     })
     .catch((err)=>{
       console.log(`there was an error sending LOGIN DATA, error: ${err}`)
+    })
+  }
+
+  function handleSignup(e) {
+    e.preventDefault();
+    fetch('http://localhost:3000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username: usernameState, password: passwordState})
+    }).then(res => {
+       if (res.status == 200) {
+        navigate("/home")
+        }})
+    .then((data)=>{
+      console.log('THIS IS FROM THE RESPONSE', data)
+    })
+    .catch((err)=>{
+      console.log(`there was an error sending SIGN UP DATA, error: ${err}`)
     })
   }
 
@@ -38,18 +65,18 @@ const LoginPage =  () => {
     <div className='login-container'>
         <Typography>Login</Typography>
         <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={(e) => dispatch(updateUserPassActionCreator({username: usernameState, password:e.target.value}))}
-        />
-        <TextField
           id="outlined-username-input"
           label="Username"
           type="username"
           autoComplete="current-username"
           onChange={(e) => dispatch(updateUserPassActionCreator({username: e.target.value, password: passwordState}))}
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          onChange={(e) => dispatch(updateUserPassActionCreator({username: usernameState, password:e.target.value}))}
         />
         <div className='button-container'>
         <Button variant="contained" onClick={handleSubmit}>Login</Button>
@@ -67,28 +94,31 @@ const LoginPage =  () => {
           label="Username"
           type="username"
           autoComplete="current-username"
-          onChange={(e) => dispatch(updateUserPassActionCreator({username:e.target.value}))}
+          onChange={(e) => dispatch(updateUserPassActionCreator({username: e.target.value, password: passwordState}))}
         />
+
         <TextField
           id="outlined-password-input"
           label="Password"
           type="password"
           autoComplete="current-password"
-          onChange={(e) => dispatch(updateUserPassActionCreator({password:e.target.value}))}
+          onChange={(e) => dispatch(updateUserPassActionCreator({username: usernameState, password:e.target.value}))}
         />
         <TextField
           id="outlined-password-input"
           label="Repeat Password"
           type="password"
           autoComplete="current-password"
-          onChange={(e) => dispatch(updateUserPassActionCreator({password:e.target.value}))}
+          onChange={(e) => dispatch(updateUserPassActionCreator({username: usernameState, password:e.target.value}))}
         />
         <div className='button-container'>
-        <Button variant="contained">Sign Up</Button>
+        <Button variant="contained" onClick={handleSignup}>Sign Up</Button>
         </div>
       </div>
 
   const renderView = (showSignUp) ? signUp : logIn
+
+
 
   return (
       <>
