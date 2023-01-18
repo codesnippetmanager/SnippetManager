f990b1f5702b3260116a075d9d9bfa0769a0f8cc
Dan Pietsch <81656645+dpietsch14@users.noreply.github.com>
2023-01-18 14:27:02 -0500

    Added functionality to send code snippets to database (#13)

--- components/LoginPage.jsx
+++ components/LoginPage.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import {Button, Typography, Box, TextField} from '@mui/material';
+import {
+  Button, Typography, Box, TextField,
+} from '@mui/material';
 
-import "../styling/login.css";
+import '../styling/login.css';
 import { useDispatch, useSelector } from 'react-redux';
-import {updateUserPassActionCreator, updateSignInActionCreator, loginActionCreator} from "../redux/actions.js";
+import { updateUserPassActionCreator, updateSignInActionCreator } from '../redux/actions.js';
 
-
-
-const LoginPage =  () => {
-
-  const usernameState = useSelector(state => state.auth.username);
-  const passwordState = useSelector(state => state.auth.password);
-  const showSignUp = useSelector(state => state.auth.showSignup);
+function LoginPage() {
+  const usernameState = useSelector((state) => state.auth.username);
+  const passwordState = useSelector((state) => state.auth.password);
+  const showSignUp = useSelector((state) => state.auth.showSignup);
   const dispatch = useDispatch();
 
 
@@ -56,47 +55,45 @@ const LoginPage =  () => {
         <Button variant="contained" onClick={(e) => dispatch(updateSignInActionCreator({showSignup: true}))}>Sign Up</Button>
         </div>
     </div>
+  );
 
-
-
-  const signUp = 
-    <div className='login-container'>
-        <Typography>Sign Up</Typography>
-        <TextField
-          id="outlined-username-input"
-          label="Username"
-          type="username"
-          autoComplete="current-username"
-          onChange={(e) => dispatch(updateUserPassActionCreator({username:e.target.value}))}
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={(e) => dispatch(updateUserPassActionCreator({password:e.target.value}))}
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Repeat Password"
-          type="password"
-          autoComplete="current-password"
-          onChange={(e) => dispatch(updateUserPassActionCreator({password:e.target.value}))}
-        />
-        <div className='button-container'>
+  const signUp = (
+    <div className="login-container">
+      <Typography>Sign Up</Typography>
+      <TextField
+        id="outlined-username-input"
+        label="Username"
+        type="username"
+        autoComplete="current-username"
+        onChange={(e) => dispatch(updateUserPassActionCreator({ username: e.target.value }))}
+      />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        onChange={(e) => dispatch(updateUserPassActionCreator({ password: e.target.value }))}
+      />
+      <TextField
+        id="outlined-password-input"
+        label="Repeat Password"
+        type="password"
+        autoComplete="current-password"
+        onChange={(e) => dispatch(updateUserPassActionCreator({ password: e.target.value }))}
+      />
+      <div className="button-container">
         <Button variant="contained">Sign Up</Button>
-        </div>
       </div>
+    </div>
+  );
 
-  const renderView = (showSignUp) ? signUp : logIn
+  const renderView = (showSignUp) ? signUp : logIn;
 
   return (
-      <>
-        {renderView}
-      </>
-  )
-
-};
-
+    <>
+      {renderView}
+    </>
+  );
+}
 
 export default LoginPage;
