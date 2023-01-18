fcfb7f9246be79a7a77122c54d67fac8df5836dc
mariamzakariadze <87081739+mariamzakariadze@users.noreply.github.com>
2023-01-18 10:35:49 -0500

    Maria/login page (#6)
    * login basic UI with redux
    
    * state for snippets

--- redux/actions.js
+++ redux/actions.js
@@ -4,6 +4,22 @@ import * as types from "./actionTypes.js"
 export const updateUserPassActionCreator = inputs => {
   return ({
     type: types.UPDATE_FORM,
-    payload: inputs,    //object with key username and password
+    payload: inputs,    //object with key username and password and signup
   })
 };
+
+export const updateSignInActionCreator = inputs => {
+  return ({
+    type: types.SHOW_SIGNUP,
+    payload: inputs 
+  })
+}
+
+export const updateSnippetListActionCreator = inputs => {
+  return ({
+    type: types.ADD_SNIPPET,
+    payload: inputs // snippet : "CODE"
+  })
+
+}
+
